ab4eafae0c151cc99806242090135e1bb3a875a5
Fredrik Halvarsson <Fredrik.Halvarsson@live.se>
2023-11-13 11:59:08 +0100

    Implementerade en Loginmetod

--- Grouplab_Bank/Bank.cs
+++ Grouplab_Bank/Bank.cs
@@ -17,6 +17,45 @@
         public void Run()
         {
             Console.WriteLine("Nu kör vi :) ");
+            User LoggedUser=Login();
+            
+        }
+        public User Login()
+        {
+            bool loggedIn = false;
+            User LoggedUser=new User();
+            do
+            {
+                Console.Clear();
+                Console.WriteLine("Welcome to Dolphin Bank" +
+                    "\n\nLogin\n");
+                Console.Write("User Name: ");
+                var userNameInput=Console.ReadLine();
+                bool exists = Users.Exists(e => e.Username == userNameInput);
+                if (exists)
+                {
+                    Console.Clear();
+                    LoggedUser = Users.Find(e => e.Username == userNameInput);
+                    Console.WriteLine($"User Name: {LoggedUser.Username}\n\n");
+                    Console.Write("Password: ");
+                    var userPasswordInput = Console.ReadLine();
+                    if (LoggedUser.Password != userPasswordInput)
+                    {
+                        LoggedUser.FailedLogin++;
+                        Console.WriteLine("Loggin failed!");
+                        Console.WriteLine($"You have {3-LoggedUser.FailedLogin} attempts left");
+                        Console.ReadKey();
+                    }
+                    else
+                    {
+                        Console.WriteLine($"\nWelcome {LoggedUser.Name}!");
+                        Console.ReadKey();
+                        loggedIn=true;
+                    }
+                }
+            } while (!loggedIn);
+
+            return LoggedUser;
         }
 
     }
