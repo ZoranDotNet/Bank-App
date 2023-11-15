3249853a86665b91397975e820709361fe3b1c31
Fredrik Halvarsson <Fredrik.Halvarsson@live.se>
2023-11-15 13:20:10 +0100

    SelectAccount Method

--- Grouplab_Bank/Meny.cs
+++ Grouplab_Bank/Meny.cs
@@ -1,4 +1,5 @@
-﻿using System.Text;
+﻿using System.Security.Cryptography.X509Certificates;
+using System.Text;
 
 namespace Grouplab_Bank
 {
@@ -7,7 +8,7 @@ namespace Grouplab_Bank
         internal static void MainMenu(User user)
         {
             bool displayMain = true;
-            bool displaySub = true;
+            bool displaySub = true; 
             do
             {
                 Utilities.DisplayLogo();
@@ -30,6 +31,8 @@ namespace Grouplab_Bank
                                     balance.GetBalance(user);
                                     break;
                                 case 2://AccountInfo
+                                    Utilities.DisplayLogo();
+
                                     break;
                                 case 3://Transaction History
 
@@ -111,6 +114,46 @@ namespace Grouplab_Bank
             } while (displayMain == true);
         }
 
+        public static int BankMenu(string option1, string option2, string option3, string option4, string option5, string option6)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            Console.CursorVisible = false;
+            Console.WriteLine("\n\u001b[34mUse ⬆️ and ⬇️ to navigate and press \u001b[32mEnter / Return\u001b[34m to select:");
+            Console.WriteLine("\u001b[34mPlease make a choice\n ");
+            (int left, int top) = Console.GetCursorPosition();
+            int option = 1;
+            var decorator = "✅ \u001b[32m";
+            ConsoleKeyInfo key;
+            bool isSelected = false;
+            while (!isSelected)
+            {
+                Console.SetCursorPosition(left, top);
+                Console.WriteLine($"{(option == 1 ? decorator : "   ")}1 {option1}\u001b[34m");
+                Console.WriteLine($"{(option == 2 ? decorator : "   ")}2 {option2}\u001b[34m");
+                Console.WriteLine($"{(option == 3 ? decorator : "   ")}3 {option3}\u001b[34m");
+                Console.WriteLine($"{(option == 4 ? decorator : "   ")}4 {option4}\u001b[34m");
+                Console.WriteLine($"{(option == 5 ? decorator : "   ")}5 {option5}\u001b[34m");
+                Console.WriteLine($"{(option == 6 ? decorator : "   ")}6 {option5}\u001b[34m");
+
+                key = Console.ReadKey(false);
+                switch (key.Key)
+                {
+                    case ConsoleKey.UpArrow:
+                        option = option == 1 ? 6 : option - 1;
+                        break;
+                    case ConsoleKey.DownArrow:
+                        option = option == 6 ? 1 : option + 1;
+                        break;
+                    case ConsoleKey.Enter:
+                        isSelected = true;
+                        break;
+                }
+
+            }
+            Console.WriteLine($"\n{decorator}You selected {option}");
+            Console.CursorVisible = true;
+            return option;
+        }
         public static int BankMenu(string option1, string option2, string option3, string option4, string option5)
         {
             Console.OutputEncoding = Encoding.UTF8;
@@ -260,6 +303,40 @@ namespace Grouplab_Bank
             Console.WriteLine($"\n{decorator}You selected {option}");
             Console.CursorVisible = true;
             return option;
+        }
+        internal static BankAccount SelectAccount(User user) 
+            /*
+            Creates a selectable list of accounts.
+            Call method with -> BankAccount selectedAccount = SelectAccount(user); <-
+            After a null check and if (List.Count > 1)
+            */
+        {
+            int? count = user.BankAccounts.Count();
+            int option=0;
+            BankAccount selectedAccount;
+            if (count == 1)
+            {
+                option = 1;
+            }
+            else if (count == 2)
+            {
+                option=BankMenu(user.BankAccounts[0].AccountNumber, user.BankAccounts[1].AccountNumber);
+            }
+            else if (count == 3)
+            {
+                option=BankMenu(user.BankAccounts[0].AccountNumber, user.BankAccounts[1].AccountNumber, user.BankAccounts[2].AccountNumber);
+            }
+            else if (count == 4)
+            {
+                option=BankMenu(user.BankAccounts[0].AccountNumber, user.BankAccounts[1].AccountNumber, user.BankAccounts[2].AccountNumber, user.BankAccounts[3].AccountNumber);
+            }
+            else if (count == 5)
+            {
+                option=BankMenu(user.BankAccounts[0].AccountNumber, user.BankAccounts[1].AccountNumber, user.BankAccounts[2].AccountNumber, user.BankAccounts[3].AccountNumber, user.BankAccounts[4].AccountNumber);
+            }
+
+            selectedAccount = user.BankAccounts[option - 1];
+            return selectedAccount;
         }
     }
 }
