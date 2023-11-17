ea914c17c89859f6214eb2afa2956b3ce80358a4
Zoran Matovic <matovic@hotmail.com>
2023-11-17 11:03:34 +0100

    Made a new GetBalance

--- Grouplab_Bank/BankAccount3.cs
+++ Grouplab_Bank/BankAccount3.cs
@@ -2,7 +2,26 @@
 {
     internal partial class BankAccount
     {
-
+        public void GetBalance(User user)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            if (user.BankAccounts.Count == 0)
+            {
+                Console.WriteLine("You have no BankAccount");
+                Console.ReadKey();
+            }
+            else
+            {
+                decimal total = 0;
+                foreach (var item in user.BankAccounts)
+                {
+                    total += item.Balance;
+                    Console.WriteLine($"*  {item.AccountNumber,-8} * {item.Balance.ToString("N2"),-13} * {item.Currency,-4} * ");
+                    Console.WriteLine("******************************************************************************");
+                }
+                Console.ReadKey();
+            }
+        }
 
 
     }
