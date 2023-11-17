7cca5a2b786775d45d333042d345eed04ad95fcb
Zoran Matovic <matovic.zoran73@gmail.com>
2023-11-17 19:49:25 +0100

    Worked on ExternalTransfer Method

--- Grouplab_Bank/User.cs
+++ Grouplab_Bank/User.cs
@@ -10,7 +10,6 @@
         public bool IsLocked { get; set; } = false;
         public List<BankAccount>? BankAccounts { get; set; }
 
-
         public User()
         {
 
@@ -99,7 +98,6 @@
                 Console.ReadKey();
             }
         }
-        //needs work
         public void MakeTransfer(User user)
         {
             TransactionType typeFrom = TransactionType.Transfer_From;
@@ -107,7 +105,6 @@
             Utilities.DisplayLogo();
             if (user.BankAccounts.Count < 2)
             {
-
                 Utilities.DisplayLogo();
                 Console.WriteLine("\nYou need at least 2 Accounts to make a Transfer");
                 Console.ReadKey();
@@ -122,11 +119,11 @@
                 Utilities.DisplayLogo();
                 Console.WriteLine("Wich Account do you want to Transfer To ");
                 BankAccount accountNrTo = Menu.SelectAccount(user);
-               
+
                 Utilities.DisplayLogo();
                 Console.WriteLine("How much do You want to Transfer ");
                 decimal amount;
-                
+
                 while (!decimal.TryParse(Console.ReadLine(), out amount))
                 {
                     Console.Write("Try again.. ");
@@ -151,46 +148,50 @@
                         Console.WriteLine("You do not have sufficient funds in your account");
                         Console.ReadKey();
                     }
-                    
+
                 }
 
             }
         }
-                   
         public void MakeExternalTransfer(User userFrom, User userTo, string account)
         {
+            if (userFrom.BankAccounts.Count > 1)
+            {
+                Console.WriteLine("\n\nWich Account do you want to Transfer From ");
+            }
+            BankAccount accountNrFrom = Menu.SelectAccount(userFrom);
+            Console.WriteLine("How much do You want to Transfer ");
 
-            //Console.WriteLine("\n\nWich Account do you want to Transfer From ");
-            //string accountNrFrom = Console.ReadLine();
-            //Console.WriteLine("Wich Account do you want to Transfer To ");
-            //string accountNrTo = Console.ReadLine();
-            //Console.WriteLine("How much do You want to Transfer ");
-            //decimal amount;
-            //while (!decimal.TryParse(Console.ReadLine(), out amount))
-            //{
-            //    Console.Write("Try again.. ");
-            //}
+            decimal amount;
+            while (!decimal.TryParse(Console.ReadLine(), out amount))
+            {
+                Console.Write("Try again.. ");
+            }
 
+            var accountNrTo = userTo.BankAccounts.FirstOrDefault(x => x.AccountNumber == account);
+            TransactionType typeFrom = TransactionType.Transfer_From;
+            TransactionType typrTo = TransactionType.Transfer_To;
 
-            //            Console.WriteLine("\n\nWich Account do you want to Transfer From ");
-            //            string accountNrFrom = Console.ReadLine();
-            //            Console.WriteLine("Wich Account do you want to Transfer To ");
-            //            string accountNrTo = Console.ReadLine();
-            //            Console.WriteLine("How much do You want to Transfer ");
-            //            decimal amount;
-            //            while (!decimal.TryParse(Console.ReadLine(), out amount))
-            //            {
-            //                Console.Write("Try again.. ");
-            //            }
-
-            //            //finding users Account to Transfer from
-            //            var accountFrom = userFrom.BankAccounts.FirstOrDefault(x => x.AccountNumber == accountNrFrom);
-
-
-            //            Console.WriteLine("Wich AccounNumber do you want to Trasfer To");
-            //            string accountNr = Console.ReadLine();
-
-            //            // needs to make a method to find AccountNr that matches another user
+            if (accountNrFrom != null && accountNrTo != null)
+            {
+                if (accountNrFrom.Balance > amount)
+                {
+                    accountNrFrom.Balance -= amount;
+                    accountNrTo.Balance += amount;
+                    AddTransaction(accountNrFrom, typeFrom, amount);
+                    AddTransaction(accountNrTo, typrTo, amount);
+                }
+                else
+                {
+                    Console.WriteLine("You dont have sufficient funds in your Account");
+                    Console.ReadKey();
+                }
+            }
+            else
+            {
+                Console.WriteLine("Could not find the Account");
+                Console.ReadKey();
+            }
 
         }
         public void GetBalance(User user)
@@ -242,7 +243,7 @@
                 if (selectedAccount.Balance > withdrawAmount)
                 {
                     selectedAccount.Balance -= withdrawAmount;
-                    Addtransaction(selectedAccount, type, withdrawAmount);
+                    AddTransaction(selectedAccount, type, withdrawAmount);
                     Console.WriteLine($"You have successfully withdrawn {withdrawAmount} from account {selectedAccount.AccountNumber}");
                     Console.ReadKey();
                 }
@@ -254,7 +255,48 @@
 
             }
         }
+        public void GetTransactionHistory(User user)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
 
+            if (user.BankAccounts.Count == 0)
+            {
+                Console.WriteLine("You have no BankAccount!");
+                Console.ReadKey();
+                return;
+            }
+            foreach (BankAccount account in user.BankAccounts)
+            {
+                foreach (Transaction item in account.Transactions)
+                {
+                    Console.WriteLine($"{item.Date.ToShortDateString()} ***    {item.Amount.ToString("N2"),-13} *** {account.Currency,-8} *** {item.Type,-12} *** Account: {item.Account.AccountNumber}");
+                    Console.WriteLine("*********************************************************************************");
+                }
+            }
+            Console.ReadKey();
+        }
+        public void AddTransaction(BankAccount account, TransactionType type, decimal amount)
+        {
+            DateTime date = DateTime.Today;
+            decimal transactionAmount = 0;
+
+            //This makes it look like -1000 if it is withdraw or transfer from.
+            if (type == TransactionType.Withdraw || type == TransactionType.Transfer_From)
+            {
+                transactionAmount = amount - 2 * amount;
+            }
+            else
+            {
+                transactionAmount = amount;
+            }
+
+            Transaction transaction = new Transaction(date, account, transactionAmount, type);
+            if (account.Transactions == null)
+            {
+                account.Transactions = new List<Transaction>();
+            }
+            account.Transactions.Add(transaction);
+        }
 
 
     }
