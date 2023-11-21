8d9925af1201487bc6ac48b6df3b1379a4a03174
Zoran Matovic <matovic@hotmail.com>
2023-11-21 13:24:23 +0100

    removed partial class

--- Grouplab_Bank/User.cs
+++ Grouplab_Bank/User.cs
@@ -355,7 +355,7 @@
                 {
                     selectedAccount.Balance -= withdrawAmount;
                     AddTransaction(selectedAccount, type, withdrawAmount);
-                    Console.WriteLine($"You have successfully withdrawn {withdrawAmount} from account {selectedAccount.AccountNumber}");
+                    Console.WriteLine($"You have successfully withdrawn {withdrawAmount.ToString("N2")} from account {selectedAccount.AccountNumber}");
                     Console.ReadKey();
                 }
                 else
@@ -466,6 +466,74 @@
             {
                 Utilities.DisplayLogo();
                 Console.WriteLine("Insufficient privileges");
+            }
+        }
+
+        private decimal GetInterest(decimal loan, decimal interestRate = 0.05m)
+        {
+            decimal interest = loan * interestRate;
+            return interest;
+        }
+        public void LoanMoney(User user)
+        {
+            if (user.BankAccounts.Count == 0)
+            {
+                Utilities.DisplayLogo();
+                Console.WriteLine("\nYou have no BankAccount ");
+                Console.ReadKey();
+                return;
+            }
+
+            decimal totalSekAmount = 0;
+            decimal totalEuroAmount = 0;
+
+            foreach (var item in user.BankAccounts)
+            {
+                if (item.Currency == Currencies.Euro)
+                {
+                    totalEuroAmount = MakeExchange(Currencies.Euro, item.Balance);
+                }
+                totalSekAmount += item.Balance;
+            }
+            decimal totalAmount = totalSekAmount + totalEuroAmount;
+            decimal maxLoanAmount = totalAmount * 5;
+
+            Console.WriteLine($"You can borrow up to {maxLoanAmount.ToString("N2")} sek.");
+            Console.WriteLine("How much do you want to borrow?");
+            decimal loan;
+
+            TransactionType type = TransactionType.Loan;
+            while (!decimal.TryParse(Console.ReadLine(), out loan))
+            {
+                Console.Write("Try again.. ");
+                Console.ReadKey();
+            }
+
+            if (maxLoanAmount < loan)
+            {
+                Console.WriteLine("You cant loan that much money.");
+                Console.ReadKey();
+                return;
+            }
+            else
+            {
+                decimal interest = GetInterest(loan);
+                Console.WriteLine($"You have to pay {interest.ToString("N2")} kr in interest on your loan yearly");
+                Console.ReadKey();
+
+                Utilities.DisplayLogo();
+                if (user.BankAccounts.Count > 1)
+                {
+                    Console.WriteLine("Choose bankaccount to wich your loan will be deposited.");
+                    Console.ReadKey();
+                }
+
+                BankAccount selectedAccount = Menu.SelectAccount(user);
+
+                selectedAccount.Balance += loan;
+                AddTransaction(selectedAccount, type, loan);
+                Console.WriteLine($"Your {loan.ToString("N2")} kr loan has been approved and deposited to account {selectedAccount.AccountNumber}");
+                Console.ReadKey();
             }
         }
     }
